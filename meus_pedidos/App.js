430598e3690df5053d773286c79794e62d95eefc
Orlando <orlando.santos@arapiraca.ufal.br>
2022-06-14 02:12:18 -0300

    Atualização

--- meus_pedidos/App.js
+++ meus_pedidos/App.js
@@ -9,20 +9,25 @@ import {View,
         TouchableOpacity} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Estilo from './estilos/estilo';
 //importando biblioteca para trabalhar com icones:
 import Icon from 'react-native-vector-icons/AntDesign'
+
 //importando as tela de navegação:
 import TelaHome from './componentes/TelaHome';
 import TelaPedidos from './componentes/TelaPedidos'
 import CadastrarPedidos from './componentes/CadastrarPedidos';
-import VisualizarPedidos from './componentes/visualizarPedidos';
-import EditarPedidos from './componentes/editarPedidos';
+import VisualizarPedidos from './componentes/VisualizarPedidos';
+import EditarPedidos from './componentes/EditarPedidos';
 
 
 
 //Faz o retorno da pilha de navegção para o Pilha:
 const Pilha = createNativeStackNavigator();
+
+//Para utilizar o tabNavigator
+const Guias = createBottomTabNavigator();
 
 export default function App(){
       return(
